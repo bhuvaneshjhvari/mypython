89326defb4bea6c31bc7f9d8db40b68cf7cfea00
bhuvaneshjhvari <bhuvi0936@gmail.com>
2025-11-10 16:36:31 +0530

    Create arthamic.py

---
+++ arthamic.py
@@ -0,0 +1,12 @@
+age=int(input("enter the age"))
+name=(input("enter the name"))
+weight=float(intput("entre the weight"))
+add=age+weight
+sub=age-weight
+div=age/weight
+div2=age//weight
+div3=age%weight
+mil=age*weight
+print(add,sub,mil,div)
+print(div2)
+print(div3)
